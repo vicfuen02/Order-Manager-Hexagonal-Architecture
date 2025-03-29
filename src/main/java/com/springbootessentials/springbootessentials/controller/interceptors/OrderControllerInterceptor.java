d1b77e31f433e9b6c96f0cbefe4802d733c024eb
vicfuen <vicfuen02@gmail.com>
2025-03-29 20:13:48 +0100

    [JIRA-9] - InterceptorAndFilter

---
+++ src/main/java/com/springbootessentials/springbootessentials/controller/interceptors/OrderControllerInterceptor.java
@@ -0,0 +1,32 @@
+package com.springbootessentials.springbootessentials.controller.interceptors;
+
+import com.springbootessentials.springbootessentials.service.order.impl.OrderAsyncServiceImpl;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+@Component
+public class OrderControllerInterceptor implements HandlerInterceptor {
+
+    private static final Logger log = LogManager.getLogger(OrderControllerInterceptor.class);
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        log.info("pre handling order controller calls");
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        log.info("post handling order controller calls");
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        log.info("after completition handling order controller calls");
+    }
+}
